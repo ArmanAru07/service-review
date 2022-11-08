20d03a2e0e41f9154d42b04dc9907d481d86f4f1
ArmanAru <armanhosen768@gmail.com>
2022-11-08 11:59:56 +0600

    header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>this is header</h2>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <Link to={"/"} className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+            </div>
+            <div className="flex-1">
+                <Link to={"/services"} className="mr-5">Services</Link>
+                <Link to={"/review"}>Review</Link>
+            </div>
+            <div className="flex-none gap-2">
+
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt='' src="https://placeimg.com/80/80/people" />
+                        </div>
+                    </label>
+                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a href='/' className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a href='/'>Settings</a></li>
+                        <li><a href='/'>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
